98d48e14dfa85149d4ff664db61cd2498c9926b2
adambb9 <55655523+adambb9@users.noreply.github.com>
2021-09-12 09:06:55 +0200

    screen set up

--- main.py
+++ main.py
@@ -1,1 +1,49 @@
 # Lets write the pong game
+from turtle import Screen, Turtle
+import time
+
+
+
+
+screen  = Screen()
+screen.setup(width=1000, height=600)
+screen.bgcolor("black")
+screen.title("Pong 2021")
+screen.tracer(0)
+
+border = Turtle()
+#border.hideturtle()
+border.penup()
+border.color("white")
+border.pensize(5)
+border.goto(0, -290)
+border.setheading(90)
+for i in range(20):
+    border.pendown()
+    border.forward(15)
+    border.penup()
+    border.forward(15)
+
+
+
+
+
+#screen.listen()
+#screen.onkey(key="Up", fun=snake.up)
+#screen.onkey(key="Down", fun=snake.down)
+#screen.onkey(key="Left", fun=snake.left)
+#screen.onkey(key="Right", fun=snake.right)
+
+
+#game_is_on = True
+
+#while game_is_on:
+    #screen.update()
+    #time.sleep(0.2)
+    
+
+
+
+
+screen.exitonclick()
+
