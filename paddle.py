0dbd0889512489f8cbcfb0f3310f4ea8bf7a2d75
adambb9 <55655523+adambb9@users.noreply.github.com>
2021-09-19 14:32:35 +0200

    moving paddle

--- paddle.py
+++ paddle.py
@@ -9,31 +9,57 @@ MOVEDISTANCE = 20
 #create a paddle
 #paddle will be made up of a stretched square
 
-class Paddle:
-
+class Paddle(Turtle):
 
     def __init__(self, x_pos):
-        self.paddle = []
-        self.x_pos = x_pos
-        self.y_pos = 0
-        self.build_paddle()
-        
+        self.xpos = x_pos
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.shapesize(5, 1, 1)
+        self.color("white")
+    
+    def move_up(self):
+        current_ypos = self.ycor()
+        new_ypos = current_ypos + 20
+        self.goto(self.xpos, new_ypos)
 
-    def build_paddle(self):
-        new_paddle = Turtle("square")
-        new_paddle.penup()
-        new_paddle.color("white")
-        new_paddle.shapesize(5, 1, 1)
-        new_paddle.goto(self.x_pos, 0)
-        
+    def move_down(self):
+        current_ypos = self.ycor()
+        new_ypos = current_ypos - 20
+        self.goto(self.xpos, new_ypos)
+
+
     
 
-    #def build_paddle(self, length):
-        #for i in range(length):
-            #new_paddle = Turtle("square")
-            #new_paddle.penup()
-            #new_paddle.color("white")
-            #self.paddle.append(new_paddle)
-            #if len(self.paddle) == 0:
-            #new_paddle.setpos((self.x_pos - (self.paddle.index(new_paddle)*20)), 0)
+    
+        
+
+        
+
+
+
+    
+        
+
+    
+
+
+
+    
+    
+
+
+
+
+
+        
+
+
+
+
+
+
+
+   
 
